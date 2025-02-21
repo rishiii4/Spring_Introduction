cce49c54e68e495b35ccda2f813a8cff18f21d4b
Pranav Shrivastav <rishishrivastav2211@gmail.com>
2025-02-21 18:59:21 +0530

    [Pranav] Add. Rest Call to Show Hello Mark using Param

--- src/main/java/com/restapi/RestAPIController.java
+++ src/main/java/com/restapi/RestAPIController.java
@@ -1,6 +1,7 @@
 package com.restapi;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,12 @@ public class RestAPIController {
     @GetMapping("/hello/query")
     public String sayHello(@RequestParam("name") String name){
         return "Hello "+ name +" from BridgeLabz";
+    }
+
+    // Rest call to show hello Mark using param
+    @GetMapping("/hello/param/{name}")
+    public String sayParam(@PathVariable String name){
+        return "Hello "+name+ " from BridgeLabz!";
     }
 
 }
